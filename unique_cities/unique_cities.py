9b48aec02787c6c2ceee31a670ea1ba72c25e9a2
unknown <svetoslavbaykov55@gmail.com>
2025-02-12 11:49:18 +0200

    Unique cities found updated

--- unique_cities/unique_cities.py
+++ unique_cities/unique_cities.py
@@ -1,5 +1,12 @@
 from data.data import df
 
 unique_cities = df['City'].unique()
+num_unique_cities = len(unique_cities)
 
-print('The unique cities are:', ', '.join(map(str, unique_cities)), end='')
+unique_states = df['State'].unique()
+num_unique_states = len(unique_states)
+
+print(f"Number of unique cities: {num_unique_cities}")
+print(f"Number of unique states: {num_unique_states}")
+print("Unique cities:", ', '.join(map(str, unique_cities)))
+print("Unique states:", ', '.join(map(str, unique_states)))
