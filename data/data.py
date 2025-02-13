54d63e4a8940adb89d21fa2b80e3847e0006afd0
unknown <svetoslavbaykov55@gmail.com>
2025-02-13 12:38:30 +0200

    State with most cities visualisation implemented

--- data/data.py
+++ data/data.py
@@ -1,6 +1,9 @@
 import pandas as pd
 
 df = pd.read_csv('../addresses.csv')
+
+df = df.map(lambda x: x.strip() if isinstance(x, str) else x)
+
 df.columns = ['First Name', 'Last name', 'Street Address', 'City', 'State', 'Postal Code']
 
 missing_values = df.isna().any().any()
---
+++ visualisations/state_with_most_cities.py
@@ -0,0 +1,20 @@
+from data.data import df
+import matplotlib.pyplot as plt
+
+for column in df.columns:
+    for value in df[column].values:
+        print(value)
+
+state_counts = df['State'].value_counts()
+
+top_5_states = state_counts.head(5)
+
+plt.figure(figsize=(10, 5))
+top_5_states.plot(kind='bar', color='skyblue', edgecolor='black')
+
+plt.title("Top 5 Most Frequent States by Address Count", fontsize=14)
+plt.xlabel('State', fontsize=12)
+plt.ylabel('Number of Addresses', fontsize=12)
+plt.xticks(rotation=45)
+
+plt.show()
